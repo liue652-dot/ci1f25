7f0091776a527ee859bb0fe4d1f52d718cf74697
Shiru Liu <liue652@newschool.edu>
2025-11-25 16:24:37 -0500

    entry 10

---
+++ Entries/ten/ten.js
@@ -0,0 +1,31 @@
+let body = document.body;
+let popup = document.getElementById("popup");
+
+function showPopup(message, x, y) {
+  popup.textContent = message;
+
+  popup.style.left = x + "px";
+  popup.style.top = (y - 50) + "px";
+
+  popup.classList.add("show");
+
+  setTimeout(() => {
+    popup.classList.remove("show");
+  }, 1500);
+}
+
+document.querySelectorAll(".pill").forEach(btn => {
+  btn.addEventListener("click", () => {
+    
+    let color = btn.getAttribute("data-color");
+    let text = btn.getAttribute("data-text");
+
+    body.style.background = color;
+
+    let rect = btn.getBoundingClientRect();
+    let x = rect.left + rect.width / 2;
+    let y = rect.top;
+
+    showPopup(text, x, y);
+  });
+});
